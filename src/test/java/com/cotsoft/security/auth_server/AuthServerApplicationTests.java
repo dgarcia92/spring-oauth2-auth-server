0f9fcdb5ec3bb322e9a1677853ffd14cef782e15
David Castañeda <garcia.castaneda@hotmail.com>
2024-06-26 09:25:54 -0600

    Se creó primer servidor de autorización

---
+++ src/test/java/com/cotsoft/security/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotsoft.security.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
